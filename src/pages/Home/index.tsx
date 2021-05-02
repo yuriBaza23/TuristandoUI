589ac9f91ce9e998e1078098fc52c1132aa8a563
yuriBaza23 <yuribaza2321@gmail.com>
2021-05-02 03:12:33 -0300

    feat: Criação da tela home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const Home: React.FC = () => {
+    return(
+        <Header page='home'/>
+    );
+}
+
+export default Home;
